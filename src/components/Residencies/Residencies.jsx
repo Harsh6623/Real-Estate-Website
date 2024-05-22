4b4a69db99863f7b376786f64cb418bc9615ff23
unknown <harshborsadiya@gmail.com>
2024-05-22 17:43:45 +0530

    Create a value , getstarted, contact and footer page

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -9,13 +9,12 @@ const Residencies = () => {
   return (
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
-        <div className="r-head flexColStart">
+        <div className=" flexColStart r-head ">
           <span className="orangeText">Best Choices</span>
           <span className="primaryText">Popular Residencies</span>
         </div>
 
         <Swiper {...sliderSettings}>
-          <SliderButtons />
           {data.map((card, i) => (
             <SwiperSlide key={i}>
               <div className=" flexColStart r-card">
@@ -36,13 +35,3 @@ const Residencies = () => {
 };
 
 export default Residencies;
-
-const SliderButtons = () => {
-  const swiper = useSwiper();
-  return (
-    <div className="r-buttons">
-      <button onClick={() => swiper.slidePrev()}>&lt;</button>
-      <button onClick={() => swiper.slideNext()}>&gt;</button>
-    </div>
-  );
-};
